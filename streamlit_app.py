0229858d06127172878007dcb65eb0a898c90c65
Amira Mohamed <49735037+amira-mohamed@users.noreply.github.com>
2024-11-23 23:26:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 # Load the trained model
-model = joblib.load("Model_liver.pkl")  # including the trained model
+#model = joblib.load("Model_liver.pkl")  # including the trained model
 
 # Function to make predictions
 def predict(input_data):
@@ -31,7 +31,7 @@ age = col2.number_input("AGE", min_value=0, max_value=120, step=1)
 tb = col3.number_input("TB", min_value=0.0, max_value=10.0, step=0.1)
 db = col4.number_input("DB", min_value=0.0, max_value=10.0, step=0.1)
 alkphos = col5.number_input("Alkphos", min_value=0, max_value=1000, step=1)
-sgpt = col1.number_input("SGPT", min_value=0, max_value=100, step=1)
+sgpt = col6.number_input("SGPT", min_value=0, max_value=100, step=1)
 sgot = col2.number_input("SGOT", min_value=0, max_value=100, step=1)
 tp = col3.number_input("TP", min_value=0.0, max_value=10.0, step=0.1)
 alb = col4.number_input("ALB", min_value=0.0, max_value=10.0, step=0.1)
