a51fa48478b4fcef26c610febf8e67831d88cdc6
Amira Mohamed <49735037+amira-mohamed@users.noreply.github.com>
2024-11-24 01:23:01 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,8 +29,8 @@ col1, col2, col3, col4, col5= st.columns(5)
 
 # Inputs
 
-age = col2.number_input("AGE", min_value=0, max_value=120, step=1)
-gender = col1.selectbox("Gender", ["Male", "Female"])
+age = col1.number_input("AGE", min_value=0, max_value=120, step=1)
+gender = col2.selectbox("Gender", ["Male", "Female"])
 tb = col3.number_input("TB", min_value=0.0, max_value=10.0, step=0.1)
 db = col4.number_input("DB", min_value=0.0, max_value=10.0, step=0.1)
 alkphos = col5.number_input("Alkphos", min_value=0, max_value=1000, step=1)
@@ -38,7 +38,7 @@ sgpt = col1.number_input("SGPT", min_value=0, max_value=100, step=1)
 sgot = col2.number_input("SGOT", min_value=0, max_value=100, step=1)
 tp = col3.number_input("TP", min_value=0.0, max_value=10.0, step=0.1)
 alb = col4.number_input("ALB", min_value=0.0, max_value=10.0, step=0.1)
-ag_ratio = col5.number_input("A/G Ratio", min_value=0.0, max_value=5.0, step=0.1)
+ag/ratio = col5.number_input("A/G Ratio", min_value=0.0, max_value=5.0, step=0.1)
 
 
 # Process gender input (if needed for your model)
@@ -55,7 +55,7 @@ data = {'age' : age,
         'sgot' : sgot, 
         'tp' : tp, 
         'alb' : alb, 
-        'ag_ratio' : ag_ratio}
+        'ag/ratio' : ag_ratio}
 
 input_df = pd.DataFrame(data, index = [0])
 
