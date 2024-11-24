adfbdef78b3f438fb7eb91a8558fe013048345d5
Amira Mohamed <49735037+amira-mohamed@users.noreply.github.com>
2024-11-24 22:11:29 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,7 +63,7 @@ st.info(predict(np.array([[age,gender_type,tb,alkphos,sgpt,ag_ratio]])))
 '''
 # Button for prediction
 if st.button("Predict"):
-    result = predict(np.array([[17,2, 0.9,202,22,102]])))
+    result = predict(np.array([[age,gender_type,tb,alkphos,sgpt,ag_ratio]]))
     if result == 1:  # Assuming 1 = Disease and 0 = No Disease
         st.error("The patient is classified as having liver disease.")
     else:
