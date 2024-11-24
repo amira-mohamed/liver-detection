cce3b001afdf4f5c886abb342084514347e27fdb
Amira Mohamed <49735037+amira-mohamed@users.noreply.github.com>
2024-11-24 21:32:05 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ ag_ratio = col6.number_input("A/G Ratio", min_value=0.0, max_value=5.0, step=0.1
 # Assume 2 = Male, 1 = Female
 if gender == "Male":
     gender = 2
-    else 1
+    else: 1
 # Combine inputs into a single array
 
 data = {"age" : [age],
