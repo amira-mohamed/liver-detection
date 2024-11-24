ebdae0a390e9988decd7183b80a2b8db9c5325e3
Amira Mohamed <49735037+amira-mohamed@users.noreply.github.com>
2024-11-24 02:18:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,64 +13,7 @@ except NameError:
 import warnings
 warnings.filterwarnings('ignore')
 
-# Load Dataset
 
-df = pd.read_csv('liver_dataset.csv')
-
-df.columns = df.columns.map(str.lower)                            
-
-# replacing missing values with mean
-df['a/g ratio'].fillna(df['a/g ratio'].mean(), inplace=True)
-
-
-df.drop(['db', 'sgot', 'tp', 'alb'], axis=1, inplace=True)
-
-
-skewed_cols = ['a/g ratio','tb', 'alkphos', 'sgpt']
-
-
-for c in skewed_cols:
-    df[c] = df[c].apply('log1p')
-
-
-
-from sklearn.preprocessing import RobustScaler
-rs = RobustScaler()
-for c in df[['age', 'gender', 'tb', 'alkphos', 'sgpt', 'a/g ratio']].columns:
-    df[c] = rs.fit_transform(df[c].values.reshape(-1, 1))
-
-
-
-from sklearn.utils import resample
-#df['class'].value_counts()
-
-
-minority = df[df['class'] ==0]
-majority = df[df['class']==1]
-
-minority_upsample = resample(minority, replace=True, n_samples=majority.shape[0])
-
-df = pd.concat([minority_upsample, majority], axis=0)
-
-
-
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(df.drop('class', axis=1), df['class'], test_size=0.05, random_state=123)
-
-from sklearn.linear_model import LogisticRegression
-from sklearn.neural_network import MLPClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
-
-from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score
-
-from xgboost import XGBClassifier
-
-model = XGBClassifier(random_state=123)
-model.fit(X_train, y_train)
-#y_train_hat = model.predict(X_train)
-y_test_hat = model.predict([input_df])
 
 #------------------------------------------------------------------
 # Page Title
@@ -95,10 +38,53 @@ alkphos = col4.number_input("Alkphos", min_value=0, max_value=1000, step=1)
 sgpt = col5.number_input("Sgpt", min_value=0, max_value=100, step=1)
 ag_ratio = col6.number_input("A/G Ratio", min_value=0.0, max_value=5.0, step=0.1)
 
-
 # Process gender input (if needed for your model)
 gender = 1 if gender == "Male" else 2  # Assume 1 = Male, 0 = Female
 
+#---------------------------------------------------------------------------------
+# Load Dataset
+
+df = pd.read_csv('liver_dataset.csv')
+
+df.columns = df.columns.map(str.lower)                            
+
+# replacing missing values with mean
+df['a/g ratio'].fillna(df['a/g ratio'].mean(), inplace=True)
+
+df.drop(['db', 'sgot', 'tp', 'alb'], axis=1, inplace=True)
+
+skewed_cols = ['a/g ratio','tb', 'alkphos', 'sgpt']
+
+for c in skewed_cols:
+    df[c] = df[c].apply('log1p')
+
+from sklearn.preprocessing import RobustScaler
+rs = RobustScaler()
+for c in df[['age', 'gender', 'tb', 'alkphos', 'sgpt', 'a/g ratio']].columns:
+    df[c] = rs.fit_transform(df[c].values.reshape(-1, 1))
+
+from sklearn.utils import resample
+#df['class'].value_counts()
+
+minority = df[df['class'] ==0]
+majority = df[df['class']==1]
+
+minority_upsample = resample(minority, replace=True, n_samples=majority.shape[0])
+
+df = pd.concat([minority_upsample, majority], axis=0)
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(df.drop('class', axis=1), df['class'], test_size=0.05, random_state=123)
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score
+
+#----------------------------------------------------------------
 # Combine inputs into a single array
 
 data = {"age" : [age],
@@ -109,6 +95,13 @@ data = {"age" : [age],
         "a/g ratio" : [ag_ratio]}
 
 input_df = pd.DataFrame(data, index = [0])
+
+from xgboost import XGBClassifier
+
+model = XGBClassifier(random_state=123)
+model.fit(X_train, y_train)
+#y_train_hat = model.predict(X_train)
+y_test_hat = model.predict(input_df)
 
 # input Data
 with st.expander('Input Data'):
